80ad58d77c79a76e93188b7b977f161b3906cd44
andros21 <andrea.ros21@e.email>
2022-04-02 10:27:47 +0200

    fix(myconfig): add lower case `firefox` inside rules

--- myconfig.h
+++ myconfig.h
@@ -57,6 +57,7 @@ static const Rule rules[] = {
    { NULL,       "spterm",    NULL,            SPTAG(0),     1,           1,          0,        -1 },
    { NULL,       "spcalc",    NULL,            SPTAG(1),     1,           1,          0,        -1 },
    { "Firefox",  NULL,        NULL,            1 << 1,       0,           0,          0,        -1 },
+   { "firefox",  NULL,        NULL,            1 << 1,       0,           0,          0,        -1 },
    { "Telegram", NULL,        NULL,            1 << 2,       0,           0,          0,        -1 },
 };
 
